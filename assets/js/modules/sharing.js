95d89b58ccd4581edf679d889eb716c767e67a10
sergey.lysyh <sergey.lysyh@bigdropinc.com>
2017-04-03 16:27:34 +0300

    fixes

---
+++ assets/js/modules/sharing.js
@@ -0,0 +1,62 @@
+import "../libs/sharrre/jquery.sharrre.js";
+
+export default {
+    init() {
+        this.sharrre();
+    },
+
+    sharrre() {
+        if ($('.feed__list .fa-twitter').length) {
+            $('.feed__list .fa-twitter').sharrre({
+                share: {twitter: true},
+                url: $(this).data('url') != 'undefined' ? $(this).data('url') : '',
+                text: $(this).data('text') != 'undefined' ? $(this).data('text') : '',
+                enableHover: false,
+                enableCounter: false,
+                template: '<span></span>',
+                click: function (api, options) {
+                    console.log($(this).data('url'));
+                    console.log($(this).data('text'));
+                    api.simulateClick();
+                    api.openPopup('twitter');
+                }
+            });
+        }
+        if ($('.feed__list .fa-facebook').length) {
+            $('.feed__list .fa-facebook').sharrre({
+                share: {facebook: true},
+                url: $(this).data('url') != 'undefined' ? $(this).data('url') : '',
+                text: $(this).data('text') != 'undefined' ? $(this).data('text') : '',
+                enableHover: false,
+                enableCounter: false,
+                template: '<span></span>',
+                click: function (api, options) {
+                    console.log($(this).data('url'));
+                    api.simulateClick();
+                    api.openPopup('facebook');
+                }
+            });
+        }
+        if ($('.n-vk').length) {
+            $('.n-vk').html(VK.Share.button(window.location.href, {
+                noparse: true,
+                type: 'custom',
+                text: '<span class="fa fa-vk">'+$('.n-vk').attr('data-content')+'</span>'
+            }));
+        }
+        // if ($('.feed__list .fa-vk').length) {
+        //  $('.feed__list .fa-vk').sharrre({
+        //     share: {facebook: true},
+        //     url: $(this).data('url') != 'undefined' ? $(this).data('url') : '',
+        //     text: $(this).data('text') != 'undefined' ? $(this).data('text') : '',
+        //     enableHover: false,
+        //     enableCounter: false,
+        //     template: '<span></span>',
+        //     click: function (api, options) {
+        //       api.simulateClick();
+        //       api.openPopup('facebook');
+        //     }
+        //  })
+        // }
+    }
+}
